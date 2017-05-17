bc1f18855acfbe0236fda47a607c39a0c94c0efc
sTRengthEn <sTRengthEn>
2017-05-17 10:17:03 +0800

    张加强电影选座页面完成

---
+++ app/scripts/movie.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017-5-16.
+ */
+$(function () {
+    $(".chair_new").click(function () {
+        console.log(2)
+        // $("#chair_new_1").hide();
+        $("#chair_new_1").css("background-image","url(../app/image/chair_choose_1.png)","z-index","999")
+        // $("#chair_new_1").css("z-index","999")
+    })
+
+})
