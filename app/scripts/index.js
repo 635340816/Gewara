a72b99a5758fc776d72fbd66a3d38abc115af754
zyx <635340816@qq.com>
2017-05-18 14:39:25 +0800

    临时提交

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,3 +1,13 @@
 /**
  * Created by 宇欣 on 2017/5/15.
  */
+$(function () {
+    $("#dianying").click(function (e) {
+        $(this).css({"border-bottom":"3px solid #f36523","color": "#f36523"});
+        $("#yingyuan").css({"border-bottom":"none","color": "black"});
+    });
+    $("#yingyuan").click(function (e) {
+        $(this).css({"border-bottom":"3px solid #f36523","color": "#f36523"});
+        $("#dianying").css({"border-bottom":"none","color": "black"});
+    });
+});
